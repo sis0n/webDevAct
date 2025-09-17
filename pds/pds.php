d9170495ee106e01342bed5b5dfd40a19ac563b6
adrianoalwyn <adrianoalwyn>
2025-09-17 13:21:46 +0800

    delete notif

--- pds/pds.php
+++ pds/pds.php
@@ -178,9 +178,16 @@ if (isset($_GET['token'])) {
     <?php
     if (isset($_GET['msg']) && $_GET['msg'] === 'success') {
       echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-              Record successfully saved!
+              record successfully saved!
               <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
             </div>";
+    }
+    if (isset($_GET['msg']) && $_GET['msg'] === "deleted") {
+      echo '
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        record has been deleted.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
     }
     ?>
     <div class="card shadow">
